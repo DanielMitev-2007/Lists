2efb6c17110de4d65d1d24fed629aa0888cabb06
DanielMitev-2007 <daniel.mitev-26v@mgberon.com>
2025-03-06 18:33:30 +0200

    Add files via upload

---
+++ zaddd1/zaddd2/zaddd2/LinkedList.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zaddd2
+{
+    internal class LinkedList
+    {
+        public class Node
+        {
+            public int Data;
+            public Node Next;
+
+            public Node(int data)
+            {
+                Data = data;
+                Next = null;
+            }
+        }
+        public Node Head;
+
+        public void Append(int data)
+        {
+            Node newNode = new Node(data);
+            if (Head == null)
+            {
+                Head = newNode;
+                return;
+            }
+
+            Node temp = Head;
+            while (temp.Next != null)
+            {
+                temp = temp.Next;
+            }
+            temp.Next = newNode;
+        }
+
+        public int FindMiddle()
+        {
+            if (Head == null)
+                throw new InvalidOperationException("The list is empty!");
+
+            Node slow = Head;
+            Node fast = Head;
+
+            while (fast != null && fast.Next != null)
+            {
+                slow = slow.Next;
+                fast = fast.Next.Next;
+            }
+            return slow.Data;
+        }
+
+        public void PrintList()
+        {
+            Node temp = Head;
+            while (temp != null)
+            {
+                Console.Write(temp.Data + " ");
+                temp = temp.Next;
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ zaddd1/zaddd2/zaddd2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zaddd2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList list = new LinkedList();
+            list.Append(10);
+            list.Append(20);
+            list.Append(30);
+            list.Append(40);
+            list.Append(50);
+            list.Append(60);
+            list.Append(70);
+
+
+            Console.WriteLine("Connected list: ");
+            list.PrintList();
+
+            Console.WriteLine("The middle element is: " + list.FindMiddle());
+        }
+    }
+}
